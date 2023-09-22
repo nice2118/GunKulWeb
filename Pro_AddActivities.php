0f2b0c1fc98285f40db13a2f47ae19b2cf7d4548
= <=>
2023-09-22 08:52:41 +0700

    .

--- Pro_AddActivities.php
+++ Pro_AddActivities.php
@@ -10,6 +10,7 @@
 include("DB_Include.php");
 include("DB_Setup.php");
 include("Fn_AddGallery.php");
+include("Fn_Permission.php");
 
 echo '<script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>';
 
@@ -86,6 +87,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   $Title = mysqli_real_escape_string($conn, $Title);
   $Summary = mysqli_real_escape_string($conn, $Summary);
   // ทำอย่างอื่นๆ เช่นบันทึกข้อมูลลงฐานข้อมูล
+  if (CheckAdmin($globalCurrentUser) || CheckManage($globalCurrentUser)){ 
+    $DefaultActive = 1;
+  }
   $sql = "INSERT INTO `Activities` (`AT_Code`, `AT_Entity No.`, `AT_Date`, `AT_Time`, `AT_Title`, `AT_Description`, `AT_Note`, `AT_Image`, `AT_Active`, `AT_UserCreate`, `AT_CreateDate`, `AT_ModifyDate`) VALUES (NULL, $CategoryID, '$DateAddNewsFormatted', CURRENT_TIME(), '$Title', '$Summary', '$Summernote', '$newnFullNameImage', '$DefaultActive', '$globalCurrentUser' , CURRENT_TIMESTAMP,CURRENT_TIMESTAMP)";
   // ดำเนินการ INSERT ข้อมูล
   if ($conn->query($sql) === true) {
